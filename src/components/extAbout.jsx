0cf053f9c0a521a98c17d9abffd4ed1fb33f40d0
Ab9 <106424063+Ab369@users.noreply.github.com>
2025-01-10 15:40:48 +0530

    c3

--- src/components/extAbout.jsx
+++ src/components/extAbout.jsx
@@ -6,7 +6,7 @@ import ExpandableCard from "../ui-comp/expndCard";
 
 export default function ExtendedAbout(){
     return(
-        <div className="about2 text-white mt-40 md:mt-40 md:mb-20 p-6 md:px-12 flex flex-col md:gap-10 gap-7">
+        <div className="about2 text-white mt-56 md:mt-40 md:mb-20 p-6 md:px-12 flex flex-col md:gap-10 gap-7">
          <BlurText
             text="What we do"
             delay={30}
