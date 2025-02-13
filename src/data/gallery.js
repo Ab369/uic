78739ac672170b739b5817195abbdc1904b7b66a
Ab9 <106424063+Ab369@users.noreply.github.com>
2025-02-13 17:05:43 +0530

    c

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,153 +1,153 @@
-const gallery = [
-    {
-      title: "Bytegram",
-      thumbnail: 'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444265/uic/frc4qj0cmyiqjphls6nh.jpg', 
-    },
-    {
-      title: "Dotting the Eye",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444266/uic/zn3lgw6eamv6k1wmbd6q.jpg'
-    },
-    {
-      title: "Dotting the Eye",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/yqnredisqfoyl5hkniar.jpg'
-    },
-    {
-      title: "Quiz",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444274/uic/l7kfnfcjbprivfubdrfs.jpg'
-    },
-    {
-      title: "Expo",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444269/uic/gdws5or1vkbcjzzagr5v.jpg'
-    },
-    {
-      title: "Bytegram",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444265/uic/ignv0wgnd6opfk2cavsg.jpg'
-    },
-    {
-      title: "Dotting The Eye",
-      thumbnail: 'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444267/uic/ow1h5wckaqjjxbcgazma.jpg'
-    },
-    {
-      title: "Innowizion",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444274/uic/e1l2ko1ooim9vczig1vj.jpg'
-    },
-    {
-      title: "Quiz",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444278/uic/pgnc7onm86tpzx8smz5r.jpg'
-    },
-    {
-      title: "Innowizion",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/zfqce8bzsnhzbi2xpu1w.jpg'
-    },
-    {
-      title: "Expo",
-      thumbnail: 'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/gdws5or1vkbcjzzagr5v.jpg'
-    },
-    {
-      title: "Trash Bash",
-      thumbnail: 'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/wbghwhxqqgxrnftpls2n.jpg'
-    },
-    
-    {
-      title: "Quiz",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/gv3hekehf2aaq53oxe4p.jpg'
-    },
-    
-    {
-      title: "Trash Bash",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444278/uic/y0w1tgwhdyeamrm5fvot.jpg'
-    },
-    {
-      title: "Trash Bash",
-      thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444278/uic/yqnredisqfoyl5hkniar.jpg'
-    },
-    
-  ];
-
-  export default gallery;
-
-// import B1 from '../assets/events/BT1.jpeg'
-// import B2 from '../assets/events/B2.jpeg'
-// import D1 from '../assets/events/D1.jpeg'
-// import D2 from '../assets/events/D2.jpeg'
-// import D3 from '../assets/events/D3.jpeg'
-// import E1 from '../assets/events/E1.jpeg'
-// import E2 from '../assets/events/E2.jpeg'
-// import inn from '../assets/events/INN.jpeg'
-// import inn2 from '../assets/events/INN2.jpeg'
-// import Q1 from '../assets/events/Q1.jpeg'
-// import Q2 from '../assets/events/Q2.jpeg'
-// import Q3 from '../assets/events/Q3.jpeg'
-// import Q4 from '../assets/events/Q4.jpeg'
-// import Q5 from '../assets/events/Q5.jpeg'
-// import T1 from '../assets/events/T1.jpeg'
-// import T2 from '../assets/events/T2.jpeg'
-// import T3 from '../assets/events/T3.jpeg'
-
 // const gallery = [
 //     {
 //       title: "Bytegram",
-//       thumbnail: B1   
+//       thumbnail: 'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444265/uic/frc4qj0cmyiqjphls6nh.jpg', 
 //     },
 //     {
 //       title: "Dotting the Eye",
-//       thumbnail:D1
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444266/uic/zn3lgw6eamv6k1wmbd6q.jpg'
 //     },
 //     {
 //       title: "Dotting the Eye",
-//       thumbnail:D3
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/yqnredisqfoyl5hkniar.jpg'
 //     },
 //     {
 //       title: "Quiz",
-//       thumbnail: Q2
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444274/uic/l7kfnfcjbprivfubdrfs.jpg'
 //     },
 //     {
 //       title: "Expo",
-//       thumbnail:E1
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444269/uic/gdws5or1vkbcjzzagr5v.jpg'
 //     },
 //     {
 //       title: "Bytegram",
-//       thumbnail:B2
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444265/uic/ignv0wgnd6opfk2cavsg.jpg'
 //     },
 //     {
 //       title: "Dotting The Eye",
-//       thumbnail: D2
+//       thumbnail: 'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444267/uic/ow1h5wckaqjjxbcgazma.jpg'
 //     },
 //     {
 //       title: "Innowizion",
-//       thumbnail:inn
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444274/uic/e1l2ko1ooim9vczig1vj.jpg'
 //     },
 //     {
 //       title: "Quiz",
-//       thumbnail:Q5
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444278/uic/pgnc7onm86tpzx8smz5r.jpg'
 //     },
 //     {
 //       title: "Innowizion",
-//       thumbnail:inn2
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/zfqce8bzsnhzbi2xpu1w.jpg'
 //     },
 //     {
 //       title: "Expo",
-//       thumbnail: E2
+//       thumbnail: 'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/gdws5or1vkbcjzzagr5v.jpg'
 //     },
 //     {
 //       title: "Trash Bash",
-//       thumbnail: T3
+//       thumbnail: 'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/wbghwhxqqgxrnftpls2n.jpg'
 //     },
     
 //     {
 //       title: "Quiz",
-//       thumbnail:Q1
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444280/uic/gv3hekehf2aaq53oxe4p.jpg'
 //     },
     
 //     {
 //       title: "Trash Bash",
-//       thumbnail:T1
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444278/uic/y0w1tgwhdyeamrm5fvot.jpg'
 //     },
 //     {
 //       title: "Trash Bash",
-//       thumbnail:T2
+//       thumbnail:'https://res.cloudinary.com/dewsjftxy/image/upload/v1739444278/uic/yqnredisqfoyl5hkniar.jpg'
 //     },
     
 //   ];
 
 //   export default gallery;
+
+import B1 from '../assets/events/BT1.jpeg'
+import B2 from '../assets/events/B2.jpeg'
+import D1 from '../assets/events/D1.jpeg'
+import D2 from '../assets/events/D2.jpeg'
+import D3 from '../assets/events/D3.jpeg'
+import E1 from '../assets/events/E1.jpeg'
+import E2 from '../assets/events/E2.jpeg'
+import inn from '../assets/events/INN.jpeg'
+import inn2 from '../assets/events/INN2.jpeg'
+import Q1 from '../assets/events/Q1.jpeg'
+import Q2 from '../assets/events/Q2.jpeg'
+import Q3 from '../assets/events/Q3.jpeg'
+import Q4 from '../assets/events/Q4.jpeg'
+import Q5 from '../assets/events/Q5.jpeg'
+import T1 from '../assets/events/T1.jpeg'
+import T2 from '../assets/events/T2.jpeg'
+import T3 from '../assets/events/T3.jpeg'
+
+const gallery = [
+    {
+      title: "Bytegram",
+      thumbnail: B1   
+    },
+    {
+      title: "Dotting the Eye",
+      thumbnail:D1
+    },
+    {
+      title: "Dotting the Eye",
+      thumbnail:D3
+    },
+    {
+      title: "Quiz",
+      thumbnail: Q2
+    },
+    {
+      title: "Expo",
+      thumbnail:E1
+    },
+    {
+      title: "Bytegram",
+      thumbnail:B2
+    },
+    {
+      title: "Dotting The Eye",
+      thumbnail: D2
+    },
+    {
+      title: "Innowizion",
+      thumbnail:inn
+    },
+    {
+      title: "Quiz",
+      thumbnail:Q5
+    },
+    {
+      title: "Innowizion",
+      thumbnail:inn2
+    },
+    {
+      title: "Expo",
+      thumbnail: E2
+    },
+    {
+      title: "Trash Bash",
+      thumbnail: T3
+    },
+    
+    {
+      title: "Quiz",
+      thumbnail:Q1
+    },
+    
+    {
+      title: "Trash Bash",
+      thumbnail:T1
+    },
+    {
+      title: "Trash Bash",
+      thumbnail:T2
+    },
+    
+  ];
+
+  export default gallery;
